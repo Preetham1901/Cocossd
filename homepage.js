0771a7b80793ae01a766a0b8a41520ef095fc433
Preetham1901 <110259664+Preetham1901@users.noreply.github.com>
2023-02-27 17:38:15 -0600

    Add files via upload

---
+++ homepage.js
@@ -0,0 +1,20 @@
+function bedroom()
+{
+    window.location = "bedroom.html";
+}
+function tv()
+{
+    window.location = "bedroom.html";
+}
+function desk()
+{
+    window.location = "bedroom.html";
+}
+function bottle()
+{
+    window.location = "bedroom.html";
+}
+function fruit()
+{
+    window.location = "bedroom.html";
+}
